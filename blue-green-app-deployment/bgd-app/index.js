285762fa9083054c0bcb72d46d78ffb023588ebc
rocky <imrocky1976@qq.com>
2019-05-08 10:40:40 +0800

    Update index.js

--- blue-green-app-deployment/bgd-app/index.js
+++ blue-green-app-deployment/bgd-app/index.js
@@ -44,4 +44,20 @@ app.all('*', function(req, res) {   // serve all other requests
 
 });
 
-var server = http.createServer(app
+var server = http.createServer(app);
+var boot = function () {
+  server.listen(app.get('port'), function(){
+    console.info('Blue-Green-App-Test listening on port ' + app.get('port'));
+  });
+}
+var shutdown = function() {
+  server.close();
+}
+if (require.main === module) {
+  boot();
+} else {
+  console.info('Running app as a module')
+  exports.boot = boot;
+  exports.shutdown = shutdown;
+  exports.port = app.get('port');
+}
